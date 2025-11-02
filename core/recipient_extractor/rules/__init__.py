9567bd503fc09888ccc84576d0ada421370b557c
Your Name <kkr2652199@example.com>
2025-11-02 20:28:21 +0900

    refactor(recipient): Modularize priority rule stages into separate classes

---
+++ core/recipient_extractor/rules/__init__.py
@@ -0,0 +1,15 @@
+"""Recipient extractor priority rule modules."""
+
+from .base_rules import BasePriorityRule, RuleContext, RuleExecutionError
+from .priority_stage_one import PriorityStageOneRule
+from .priority_stage_two import PriorityStageTwoRule
+
+__all__ = [
+    "BasePriorityRule",
+    "RuleContext",
+    "RuleExecutionError",
+    "PriorityStageOneRule",
+    "PriorityStageTwoRule",
+]
+
+
