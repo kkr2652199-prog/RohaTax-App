47ac3a422405d109c14feacb775fa9a7dc1085ad
Your Name <kkr2652199@example.com>
2025-11-15 14:48:38 +0900

    ﻿refactor(extractor): ?ㅼ젙 諛??ㅼ썙??濡쒖쭅??config_loader 紐⑤뱢濡?遺꾨━
    - second_priority_handler.py??__init__ 硫붿꽌?쒖뿉 ?섎뱶肄붾뵫?섏뼱 ?덈뜕 紐⑤뱺 ?ㅼ젙媛믨낵 ?ㅼ썙??由ъ뒪?몃? ?낅┰?곸씤 core/recipient_extractor/utils/config_loader.py 紐⑤뱢濡?遺꾨━??
    
    - __init__ 硫붿꽌?쒕뒗 ?댁젣 ?몃? ?⑥닔瑜??몄텧?섏뿬 ?ㅼ젙??濡쒕뱶?섎뒗 ?⑥닚????븷留??섑뻾?섍쾶 ??
    
    - ?대뒗 second_priority_handler.py 由ы뙥?좊쭅????踰덉㎏ ?④퀎濡? ?ㅼ젙 愿由ъ쓽 以묒븰?붾? ?듯빐 肄붾뱶???좎?蹂댁닔?깆쓣 ?μ긽?쒗궡.
    
    - '?멸컙 寃利????듯빐 湲곕뒫???뚭?媛 ?놁쓬???뺤씤??

--- core/recipient_extractor/second_priority_handler.py
+++ core/recipient_extractor/second_priority_handler.py
@@ -35,6 +35,7 @@ from .utils.sheet_detector import (
     has_email_in_business_number_column,
     has_standard_headers,
 )
+from .utils.config_loader import load_keywords, load_config
 
 logger = logging.getLogger(__name__)
 
@@ -43,41 +44,19 @@ class SecondPriorityHandler:
     
     def __init__(self):
         self.logger = logger
-        # 2순위 검열 전용 필수 컬럼 (배달대행사 공급받는자 절대지침 반영)
-        # 5형제: 사업자등록번호, 상호, 대표명, 사업장주소, 사업자이메일
-        self.required_columns = ['사업자등록번호', '상호', '대표명', '사업장주소', '사업자이메일']
-        self.amount_columns = ['공급가액', '부가세', '요금합계']  # 아빠값(공급가액/요금합계), 엄마값(부가세)
         
-        # 배달대행사 공급받는자 절대지침 키워드 세트 (상호명)
-        self.store_keywords = [
-            '가맹점', '상호', '매장', '점포', '상점', '업체', '사업체', '매장명', '점포명', '상호명', '업소명', 
-            '사업장명', '가게', '가게명', '업소', '사업장', '점', '상점명', '가맹점명', '업체명', '사업체명'
-        ]
+        # 설정값 로드
+        config = load_config()
+        self.required_columns = config['required_columns']
+        self.amount_columns = config['amount_columns']
         
-        self.korean_cities = [
-            '서울특별시', '서울시', '부산광역시', '부산시', '대구광역시', '대구시', '인천광역시', '인천시', 
-            '광주광역시', '광주시', '대전광역시', '대전시', '울산광역시', '울산시', '세종특별자치시', '세종시', 
-            '경기도', '강원도', '충청북도', '충청남도', '전라북도', '전라남도', '경상북도', '경상남도', 
-            '제주특별자치도', '제주도', '서울', '부산', '대구', '인천', '광주', '대전', '울산', '세종'
-        ]
-        
-        self.email_domains = [
-            'naver.com', 'daum.net', 'gmail.com', 'nate.com', 'yahoo.com', 'hotmail.com', 'outlook.com', 
-            'hanmail.net', 'kakao.com', 'tistory.com', 'live.com', 'msn.com', 'icloud.com', 'me.com', 
-            'mac.com', 'aol.com', 'zoho.com', 'protonmail.com'
-        ]
-        
-        self.korean_surnames = [
-            '김', '이', '박', '최', '정', '강', '조', '윤', '장', '임', '한', '오', '서', '신', '권', '황', '안', '송', '전', '고', 
-            '문', '양', '손', '배', '조', '백', '허', '유', '남', '심', '노', '정', '하', '곽', '성', '차', '주', '우', '구', '신', 
-            '원', '태', '나', '전', '민', '유', '진', '지', '엄', '채', '천', '양', '공', '현', '방', '변', '여', '추', '노', '도', '소'
-        ]
-        
-        self.foreign_names = [
-            'John', 'David', 'Michael', 'James', 'Robert', 'William', 'Richard', 'Charles', 'Thomas', 'Christopher', 
-            'Daniel', 'Matthew', 'Anthony', 'Mark', 'Donald', 'Steven', 'Paul', 'Andrew', 'Joshua', 'Kenneth', 
-            'Kevin', 'Brian', 'George', 'Timothy', 'Ronald', 'Jason', 'Edward', 'Jeffrey', 'Ryan', 'Jacob', 'Gary', 'Nicholas'
-        ]
+        # 키워드 리스트 로드
+        keywords = load_keywords()
+        self.store_keywords = keywords['store']
+        self.korean_cities = keywords['korean_cities']
+        self.email_domains = keywords['email_domains']
+        self.korean_surnames = keywords['korean_surnames']
+        self.foreign_names = keywords['foreign_names']
         
     def is_second_priority_sheet(self, df: pd.DataFrame, column_names: List[str]) -> bool:
         """2순위 시트 감지 로직 (외부 모듈 호출)"""
